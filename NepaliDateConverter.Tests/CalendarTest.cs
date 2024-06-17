7837f59d7ca5f07e8b9fe55d257828c227f86829
Amish <amish@ekbana.com>
2024-06-17 13:49:55 +0545

    feat: updated unit tests to NUnit 4

--- NepaliDateConverter.Tests/CalendarTest.cs
+++ NepaliDateConverter.Tests/CalendarTest.cs
@@ -1,5 +1,4 @@
 using NepaliDateConverter;
-using NUnit.Framework;
 
 namespace Tests
 {
@@ -23,16 +22,16 @@ namespace Tests
         [Test]
         public void TestIsLeapYearShouldReturnTrue()
         {
-            Assert.IsTrue(calendar.IsLeapYear(1904));
-            Assert.IsTrue(calendar.IsLeapYear(1908));
-            Assert.IsTrue(calendar.IsLeapYear(1912));
-            Assert.IsTrue(calendar.IsLeapYear(1916));
-            Assert.IsTrue(calendar.IsLeapYear(2000));
-            Assert.IsTrue(calendar.IsLeapYear(2004));
-            Assert.IsTrue(calendar.IsLeapYear(2008));
-            Assert.IsTrue(calendar.IsLeapYear(2012));
-            Assert.IsTrue(calendar.IsLeapYear(2016));
-            Assert.IsTrue(calendar.IsLeapYear(2020));
+            Assert.That(calendar.IsLeapYear(1904),Is.True);
+            Assert.That(calendar.IsLeapYear(1908),Is.True);
+            Assert.That(calendar.IsLeapYear(1912),Is.True);
+            Assert.That(calendar.IsLeapYear(1916),Is.True);
+            Assert.That(calendar.IsLeapYear(2000),Is.True);
+            Assert.That(calendar.IsLeapYear(2004),Is.True);
+            Assert.That(calendar.IsLeapYear(2008),Is.True);
+            Assert.That(calendar.IsLeapYear(2012),Is.True);
+            Assert.That(calendar.IsLeapYear(2016),Is.True);
+            Assert.That(calendar.IsLeapYear(2020),Is.True);
         }
 
         [Test]
@@ -41,61 +40,61 @@ namespace Tests
             bool isLeapYear2017 = calendar.IsLeapYear(2017);
             bool isLeapYear2018 = calendar.IsLeapYear(2018);
             bool isLeapYear2025 = calendar.IsLeapYear(2025);
-            Assert.IsFalse(isLeapYear2017);
-            Assert.IsFalse(isLeapYear2018);
-            Assert.IsFalse(isLeapYear2025);
+            Assert.That(isLeapYear2017,Is.False);
+            Assert.That(isLeapYear2018,Is.False);
+            Assert.That(isLeapYear2025, Is.False);
         }
 
         // GetDayOfWeek()
         [Test]
         public void TestGetDayOfWeekShouldReturnDayOfWeek()
         {
-            Assert.AreEqual("Sunday", calendar.GetDayOfWeek(0));
-            Assert.AreEqual("Sunday", calendar.GetDayOfWeek(1));
-            Assert.AreEqual("Monday", calendar.GetDayOfWeek(2));
-            Assert.AreEqual("Tuesday", calendar.GetDayOfWeek(3));
-            Assert.AreEqual("Wednesday", calendar.GetDayOfWeek(4));
-            Assert.AreEqual("Thursday", calendar.GetDayOfWeek(5));
-            Assert.AreEqual("Friday", calendar.GetDayOfWeek(6));
-            Assert.AreEqual("Saturday", calendar.GetDayOfWeek(7));
+            Assert.That("Sunday", Is.EqualTo(calendar.GetDayOfWeek(0)));
+            Assert.That("Sunday", Is.EqualTo(calendar.GetDayOfWeek(1)));
+            Assert.That("Monday", Is.EqualTo(calendar.GetDayOfWeek(2)));
+            Assert.That("Tuesday", Is.EqualTo(calendar.GetDayOfWeek(3)));
+            Assert.That("Wednesday", Is.EqualTo(calendar.GetDayOfWeek(4)));
+            Assert.That("Thursday", Is.EqualTo(calendar.GetDayOfWeek(5)));
+            Assert.That("Friday", Is.EqualTo(calendar.GetDayOfWeek(6)));
+            Assert.That("Saturday", Is.EqualTo(calendar.GetDayOfWeek(7)));
         }
 
         // GetEnglishMonth()
         [Test]
         public void TestGetEnglishMonthShouldReturnEnglishMonthName()
         {
-            Assert.AreEqual("January", calendar.GetEnglishMonth(0));
-            Assert.AreEqual("January", calendar.GetEnglishMonth(1));
-            Assert.AreEqual("February", calendar.GetEnglishMonth(2));
-            Assert.AreEqual("March", calendar.GetEnglishMonth(3));
-            Assert.AreEqual("April", calendar.GetEnglishMonth(4));
-            Assert.AreEqual("May", calendar.GetEnglishMonth(5));
-            Assert.AreEqual("June", calendar.GetEnglishMonth(6));
-            Assert.AreEqual("July", calendar.GetEnglishMonth(7));
-            Assert.AreEqual("August", calendar.GetEnglishMonth(8));
-            Assert.AreEqual("September", calendar.GetEnglishMonth(9));
-            Assert.AreEqual("October", calendar.GetEnglishMonth(10));
-            Assert.AreEqual("November", calendar.GetEnglishMonth(11));
-            Assert.AreEqual("December", calendar.GetEnglishMonth(12));
+            Assert.That("January", Is.EqualTo(calendar.GetEnglishMonth(0)));
+            Assert.That("January", Is.EqualTo(calendar.GetEnglishMonth(1)));
+            Assert.That("February", Is.EqualTo(calendar.GetEnglishMonth(2)));
+            Assert.That("March", Is.EqualTo(calendar.GetEnglishMonth(3)));
+            Assert.That("April", Is.EqualTo(calendar.GetEnglishMonth(4)));
+            Assert.That("May", Is.EqualTo(calendar.GetEnglishMonth(5)));
+            Assert.That("June", Is.EqualTo(calendar.GetEnglishMonth(6)));
+            Assert.That("July", Is.EqualTo(calendar.GetEnglishMonth(7)));
+            Assert.That("August", Is.EqualTo(calendar.GetEnglishMonth(8)));
+            Assert.That("September", Is.EqualTo(calendar.GetEnglishMonth(9)));
+            Assert.That("October", Is.EqualTo(calendar.GetEnglishMonth(10)));
+            Assert.That("November", Is.EqualTo(calendar.GetEnglishMonth(11)));
+            Assert.That("December", Is.EqualTo(calendar.GetEnglishMonth(12)));
         }
 
         // GetNepaliMonth()
         [Test]
         public void TestGetNepaliMonthShouldReturnNepaliMonthName()
         {
-            Assert.AreEqual("Baishakh", calendar.GetNepaliMonth(0));
-            Assert.AreEqual("Baishakh", calendar.GetNepaliMonth(1));
-            Assert.AreEqual("Jestha", calendar.GetNepaliMonth(2));
-            Assert.AreEqual("Ashad", calendar.GetNepaliMonth(3));
-            Assert.AreEqual("Shrawan", calendar.GetNepaliMonth(4));
-            Assert.AreEqual("Bhadra", calendar.GetNepaliMonth(5));
-            Assert.AreEqual("Aswin", calendar.GetNepaliMonth(6));
-            Assert.AreEqual("Kartik", calendar.GetNepaliMonth(7));
-            Assert.AreSame("Mangshir", calendar.GetNepaliMonth(8));
-            Assert.AreEqual("Poush", calendar.GetNepaliMonth(9));
-            Assert.AreEqual("Magh", calendar.GetNepaliMonth(10));
-            Assert.AreEqual("Falgun", calendar.GetNepaliMonth(11));
-            Assert.AreEqual("Chaitra", calendar.GetNepaliMonth(12));
+            Assert.That("Baishakh", Is.EqualTo(calendar.GetNepaliMonth(0)));
+            Assert.That("Baishakh", Is.EqualTo(calendar.GetNepaliMonth(1)));
+            Assert.That("Jestha", Is.EqualTo(calendar.GetNepaliMonth(2)));
+            Assert.That("Ashad", Is.EqualTo(calendar.GetNepaliMonth(3)));
+            Assert.That("Shrawan", Is.EqualTo(calendar.GetNepaliMonth(4)));
+            Assert.That("Bhadra", Is.EqualTo(calendar.GetNepaliMonth(5)));
+            Assert.That("Aswin", Is.EqualTo(calendar.GetNepaliMonth(6)));
+            Assert.That("Kartik", Is.EqualTo(calendar.GetNepaliMonth(7)));
+            Assert.That("Mangshir", Is.EqualTo(calendar.GetNepaliMonth(8)));
+            Assert.That("Poush", Is.EqualTo(calendar.GetNepaliMonth(9)));
+            Assert.That("Magh", Is.EqualTo(calendar.GetNepaliMonth(10)));
+            Assert.That("Falgun", Is.EqualTo(calendar.GetNepaliMonth(11)));
+            Assert.That("Chaitra", Is.EqualTo(calendar.GetNepaliMonth(12)));
         }
 
         // ValidEnglishDate()
@@ -105,29 +104,29 @@ namespace Tests
             // Valid years
             for (int year = 1944; year <= 2033; year++)
             {
-                Assert.IsTrue(calendar.ValidEnglishDate(year, 1, 1));
+                Assert.That(calendar.ValidEnglishDate(year, 1, 1), Is.True);
             }
 
             // valid months
             for (int month = 1; month <= 12; month++)
             {
-                Assert.IsTrue(calendar.ValidEnglishDate(2017, month, 1));
+                Assert.That(calendar.ValidEnglishDate(2017, month, 1), Is.True);
             }
 
             // valid days
             for (int day = 1; day <= 31; day++)
             {
-                Assert.IsTrue(calendar.ValidEnglishDate(2017, 8, day));
+                Assert.That(calendar.ValidEnglishDate(2017, 8, day),Is.True);
             }
         }
 
         [Test]
         public void TestValidEnglishDateShouldBeInvalid()
         {
-            Assert.IsFalse(calendar.ValidEnglishDate(1943, 1, 1));
-            Assert.IsFalse(calendar.ValidEnglishDate(2017, 13, 1));
-            Assert.IsFalse(calendar.ValidEnglishDate(1943, 11, 33));
-            Assert.IsFalse(calendar.ValidEnglishDate(2034, 1, 1));
+            Assert.That(calendar.ValidEnglishDate(1943, 1, 1), Is.False);
+            Assert.That(calendar.ValidEnglishDate(2017, 13, 1), Is.False);
+            Assert.That(calendar.ValidEnglishDate(1943, 11, 33), Is.False);
+            Assert.That(calendar.ValidEnglishDate(2034, 1, 1), Is.False);
         }
 
         // ValidNepaliDate()
@@ -137,29 +136,29 @@ namespace Tests
             // Valid years
             for (int year = 2000; year <= 2089; year++)
             {
-                Assert.IsTrue(calendar.ValidNepaliDate(year, 1, 1));
+                Assert.That(calendar.ValidNepaliDate(year, 1, 1), Is.True);
             }
 
             // valid months
             for (int month = 1; month <= 12; month++)
             {
-                Assert.IsTrue(calendar.ValidNepaliDate(2074, month, 1));
+                Assert.That(calendar.ValidNepaliDate(2074, month, 1), Is.True);
             }
 
             // valid days
             for (int day = 1; day <= 32; day++)
             {
-                Assert.IsTrue(calendar.ValidNepaliDate(2074, 8, day));
+                Assert.That(calendar.ValidNepaliDate(2074, 8, day), Is.True);
             }
         }
 
         [Test]
         public void TestValidNepaliDateShouldBeInvalid()
         {
-            Assert.IsFalse(calendar.ValidNepaliDate(1999, 1, 1));
-            Assert.IsFalse(calendar.ValidNepaliDate(2090, 1, 1));
-            Assert.IsFalse(calendar.ValidNepaliDate(2074, 1, 33));
-            Assert.IsFalse(calendar.ValidNepaliDate(2090, 13, 1));
+            Assert.That(calendar.ValidNepaliDate(1999, 1, 1), Is.False);
+            Assert.That(calendar.ValidNepaliDate(2090, 1, 1), Is.False);
+            Assert.That(calendar.ValidNepaliDate(2074, 1, 33), Is.False);
+            Assert.That(calendar.ValidNepaliDate(2090, 13, 1), Is.False);
         }
     }
 }
